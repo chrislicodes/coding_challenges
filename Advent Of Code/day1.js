cb15dcd4690f094c6eb3a20a0024bef816a0809a
Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>
2020-12-02 21:36:18 +0100

    added perf_hooks to compare perf to other sol

--- Advent Of Code/day1.js
+++ Advent Of Code/day1.js
@@ -1,5 +1,3 @@
-"use strict";
-
 // * --- Day 1: Report Repair ---
 // After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island. Surely, Christmas will go on without you.
 
@@ -26,6 +24,12 @@
 
 // Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
 
+"use strict";
+const { performance } = require("perf_hooks");
+let t0 = performance.now();
+
+console.log("Starting Part 1");
+
 //prettier-ignore
 const arr = [1583,1295,1747,1628,1756,1992,1984,1990,2006,1626,1292,1561,1697,1249,2001,1822,1715,1951,1600,1615,1769,1825,1335,1987,1745,1660,1952,1437,1348,1968,615,1847,476,1346,1357,1838,1955,1750,1831,2003,1730,1696,1257,1581,866,1765,1691,1995,1977,1988,1713,1599,1300,1892,1550,2002,1694,1930,1998,1564,1704,1398,864,1480,1578,1946,1850,1964,1914,1860,1979,1857,1969,1675,1967,2009,1950,1834,783,1935,1963,1659,1314,1647,1671,1706,1734,1965,1666,316,1657,1663,1373,1719,1778,1559,1869,1958,1986,1845,1643,1783,1670,1445,1758,2008,1680,1251,1982,1420,1621,1997,1785,1994,1376,1944,1771,1844,96,467,1954,903,1368,1305,1589,1970,1980,1521,1775,1629,1796,1985,1957,1669,1637,1606,1766,1972,1956,1685,1235,58,1996,1959,1788,1273,1378,1233,1470,1584,1741,1327,1763,1989,1665,1667,1975,1862,1791,1229,1873,1761,1754,1882,1642,1971,1777,1580,1648,1678,1266,1645,502,1717,1723,1244,1370,1898,1755,1708,1983,1901,844,1239,1290,1879,1656,1966,1929,1993,1743,1909,1451,2000,1978,1938,1707,1337,1362,1263]
 
@@ -36,14 +40,23 @@ const findTwoNumbers = function (arr, num) {
   //find the complement to 2020
   for (const iter of arr) {
     if (arr.includes(num - iter)) {
-      console.log(num - iter, iter, num * iter);
-      return;
+      console.log(num - iter, iter, num, "Solution:", (num - iter) * iter);
+      // return;
     }
   }
 };
 
 findTwoNumbers(arr_even, 2020); //316 1704 538464
 findTwoNumbers(arr_odd, 2020); //none
+
+console.log(
+  "Finished Part 1, it took",
+  (performance.now() - t0) / 1000,
+  "seconds"
+);
+console.log("Starting Part 2");
+
+t0 = performance.now();
 
 // PART 2: The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
 
@@ -74,10 +87,16 @@ const findThreeNumbers = function (arr, num) {
           "Solution:",
           item1 * item2 * comp
         );
-        return;
+        // return;
       }
     }
   }
 };
 
 findThreeNumbers(arr, 2020); //615 + 903 + 502 = 2020, * = 278783190
+
+console.log(
+  "Finished Part 2, it took",
+  (performance.now() - t0) / 1000,
+  "seconds"
+);
