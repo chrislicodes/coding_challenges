f0e75eee58ae9a15e2f243805ca5f8e555f8973f
Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>
2020-12-08 23:27:19 +0100

    first appraoch day7...

--- Advent Of Code/2020/day7/day7.js
+++ Advent Of Code/2020/day7/day7.js
@@ -7,7 +7,7 @@ const { performance } = require("perf_hooks");
 
 //########################################
 
-const filePath = "./input.txt";
+const filePath = "./testInput.txt";
 
 //########################################
 
@@ -24,9 +24,57 @@ let t0 = performance.now();
 
 //########################################
 
-//Code
+const matrix = data.split(/\n/g); //.slice(0, 10);
 
-//########################################
+const bagRules = {};
+const goalRules = {};
+const endNodes = {};
+
+for (const rule of matrix) {
+  const [key, ...content] = rule.match(/\w+\s\w+(?=\s+bag)/g);
+  bagRules[key] = content;
+}
+
+for (const key of Object.keys(bagRules)) {
+  if (bagRules[key].includes("shiny gold")) goalRules[key] = bagRules[key];
+  if (bagRules[key].includes("no other")) endNodes[key] = bagRules[key];
+}
+
+// console.log(bagRules);
+// console.log(goalRules);
+// console.log(endNodes);
+
+const unpackBag = function (bag, arr) {
+  return arr[bag];
+};
+
+const unpackedArr = {};
+
+for (const key of Object.keys(bagRules)) {
+  //keeping track of the key
+  const unpacked = [];
+  bagRules[key].forEach((item, i) => {
+    if (item === "shiny gold") {
+      unpacked.push("shiny gold");
+      return;
+    }
+    if (item === "no other") {
+      unpacked.push("no other");
+      return;
+    }
+    if (item in bagRules) {
+      unpacked.push(bagRules[item]);
+    } else {
+      unpacked.push(item);
+    }
+  });
+
+  unpackedArr[key] = unpacked.flat();
+}
+console.log(bagRules);
+console.log(unpackedArr);
+
+// ########################################
 
 console.log(
   chalk.green("Part 1 took", ((performance.now() - t0) / 1000).toFixed(4), "s.")
