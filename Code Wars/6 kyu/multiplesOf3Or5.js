bb0e0ce69629d307e2181cb83fff42c3c844b108
Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>
2020-12-21 23:58:23 +0100

    solved 6kyu multiplesOf3Or5

---
+++ Code Wars/6 kyu/multiplesOf3Or5.js
@@ -0,0 +1,16 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+// Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative, return 0(for languages that do have them)
+
+"use strict";
+
+function solution(number) {
+  const arrNum = Array.from({ length: number - 1 }, (_, i) => i + 1)
+    .filter((num) => num % 3 === 0 || num % 5 === 0)
+    .reduce((acc, cum) => acc + cum);
+  return arrNum;
+}
+
+console.log(solution(10)); // 3, 5, 6, 9 => 23
