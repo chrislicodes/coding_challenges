8d8b55dbdfe73bc0c89439446c3ce698002da491
Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>
2020-12-30 12:02:21 +0100

    refactor kyu7 overTheRoad

--- Code Wars/7 kyu/overTheRoad.js
+++ Code Wars/7 kyu/overTheRoad.js
@@ -2,11 +2,17 @@
 
 // You've just moved into a perfectly straight street with exactly n identical houses on either side of the road. Naturally, you would like to find out the house number of the people on the other side of the street. The street looks something like this:
 
-const solution = function (address, n) {
-  const leftSolution = (address - 1) / 2;
-  const rightSolution = 2 * n - 2 * leftSolution;
+// const solution = function (address, n) {
+//   const leftSolution = (address - 1) / 2;
+//   const rightSolution = 2 * n - 2 * leftSolution;
 
-  return rightSolution;
+//   return rightSolution;
+// };
+
+// console.log(solution(3, 5));
+
+const solution = function (address, n) {
+  return 2 * n + 1 - address;
 };
 
 console.log(solution(3, 5));
